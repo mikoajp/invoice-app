3180175779f16ba5f8bdc1ab4863cee99edcb1ec
mikolaj <mikoajmicht@gmail.com>
2024-08-18 18:57:46 +0200

    productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,23 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function getProducts() {
-        $products = Product::orderBy('id','DESC')->get();
+        $products = Product::orderBy('id', 'DESC')->get();
         return response()->json($products);
+    }
+
+    public function createProduct(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'description' => 'nullable|string',
+        ]);
+        
+        $product = new Product();
+        $product->name = $validatedData['name'];
+        $product->price = $validatedData['price'];
+        $product->description = $request->input('description', '');
+        $product->save();
+
+        return response()->json($product, 201);
     }
 }
